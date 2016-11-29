0f8f72f279b6d47006b86746b99f24e33b4b2035
ZL <zl@cocodeMac-mini.local>
2016-11-29 18:16:03 +0800

    mac 2

--- blog.py
+++ blog.py
@@ -1,85 +1,100 @@
-# coding=utf-8
 import os
-from flask import Flask,render_template,session,redirect,url_for,flash
-from flask import make_response
-from flask.ext.bootstrap import Bootstrap
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_script import Manager,Shell
+from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask.ext.wtf import Form
-from wtforms import StringField,SubmitField
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate,MigrateCommand
+from flask.ext.mail import Mail,Message
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=\
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-app.config['SECRET_KEY']='hard to guess string'
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
-db = SQLAlchemy(app)
+
+manager = Manager(app)
+
 bootstrap = Bootstrap(app)
-moment=Moment(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+mail=Mail(app)
 
 
-
-class NameForm(Form):
-    name=StringField('What is your name',validators=[Required()])
-    submit=SubmitField('Submit')
 
 class Role(db.Model):
-    __tablename__='roles'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(64),unique=True)
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
+
 class User(db.Model):
-    __tablename__='users'
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(64),unique=True,index=True)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
-        return'<User %r>' % self.username
+        return '<User %r>' % self.username
 
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
+manager.add_command('db',MigrateCommand)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('用户名变更')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['know']=False
+        else:
+            session['know']=True
         session['name'] = form.name.data
+        form.name.data=''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('know',False))
 
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
-
-@app.route('/cookies')
-def cookies():
-    response=make_response('<h1>this document carries a cookies</h1>')
-    response.set_cookid('answer','42')
-    return response
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html')
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html')
 
 if __name__ == '__main__':
-    app.run()
+    db.create_all()
+    manager.run()
