97385f331f905e348b066bcaa75d37eacf8afb47
ZL <zl@cocodeMac-mini.local>
2016-12-02 19:04:12 +0800

    page96 mac

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -34,3 +34,32 @@ class RegistrationForm(Form):
     def validate_username(self,field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('用户名已存在')
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField('旧密码',validators=[DataRequired()])
+    password = PasswordField('新密码',validators=[DataRequired(),EqualTo('password2',message='密码不一致')])
+    password2 = PasswordField('确认密码',validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+class PasswordResetRequestForm(Form):
+    email = StringField('邮箱',validators=[DataRequired(),Length(1,64),Email()])
+    submit = SubmitField('重置密码')
+
+class PasswordResetForm(Form):
+    email = StringField('邮箱',validators=[DataRequired(),Length(1,64),Email()])
+    password = PasswordField('新密码',validators=[DataRequired(),EqualTo('password2',message='密码不一致')])
+    password2 = PasswordField('确认密码',validaotrs=[DataRequired()])
+    submit = SubmitField('重置密码')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('无效邮箱地址')
+
+class ChangeEmailForm(Form):
+    email = StringField('新邮箱',validators=[DataRequired(),Length(1,64),Email()])
+    password = PasswordField('密码',validators=[DataRequired()])
+    submit = SubmitField('邮箱地址已更改')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱地址已存在')
